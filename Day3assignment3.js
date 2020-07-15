8e9cfbde8b0c9b0260090c1516e32df78efb6aca
abhig2000 <49153588+abhig2000@users.noreply.github.com>
2020-07-15 12:56:08 +0530

    Add files via upload

---
+++ Day3assignment3.js
@@ -0,0 +1,65 @@
+// String data types:
+//  It is used to represent sequence of character.
+
+var c= 'abhishek';
+var d="gupta";
+
+console.log(c);
+console.log(d);
+
+//Number data type:
+
+var a = 25;         // integer
+var b = 80.5;       // floating-point number
+
+console.log(a);
+console.log(b);
+
+
+// Boolean data type:
+
+var isReading = true;   // yes, I'm reading
+var isSleeping = false; // no, I'm not sleeping
+
+var a = 2, b = 5, c = 10;
+ 
+console.log(b > a) // Output: true
+console.log(b > c) // Output: false
+
+// undefined data type:
+
+var a;
+var b = "Hello World!"
+ 
+alert(a) // Output: undefined
+alert(b) // Output: Hello World!
+
+//null data type
+//  it is not equal to zero.
+
+var a = null;
+alert(a);
+
+
+// Object data type
+
+var e=({name:"abhishek",age:"22"})
+var f=({name:"abhi",age:"22"})
+var g=({name:"abhi",age:"22"})
+
+console.log({e,f,g});
+
+
+//Array data type
+
+var colors = ["Red", "Yellow", "Green", "Orange"];
+var cities = ["London", "Paris", "New York"];
+ 
+alert(colors[0]);   // Output: Red
+alert(cities[2]);   // Output: New York
+
+
+
+
+
+
