4c2876580da3dc1ef5db465029c829f85204f958
abhig2000 <49153588+abhig2000@users.noreply.github.com>
2020-07-16 23:56:53 +0530

    Add files via upload

---
+++ day3assignment2.js
@@ -0,0 +1,7 @@
+function osversion(os,version){
+    console.log(`The OS name is ${os} and Version ${version}.`)
+}
+
+let os = prompt("Enter the Os");
+let version = prompt("Enter the version");
+osversion(os,version);
