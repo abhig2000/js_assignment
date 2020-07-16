f86f66a928882ae770751a47a4711bb6bbac185e
abhig2000 <49153588+abhig2000@users.noreply.github.com>
2020-07-16 12:18:36 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,6 @@
+console.log("Assignment 1")
+
+let firstname = prompt("Enter Your FirstName");
+let lastname = prompt("Enter Your LastName");
+
+console.log(`Good Morning ${firstname} ${lastname}.`);
