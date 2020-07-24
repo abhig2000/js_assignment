d17f90693d05ce152a7d9c72d3e9b989d079df26
abhig2000 <49153588+abhig2000@users.noreply.github.com>
2020-07-25 00:23:01 +0530

    Add files via upload

---
+++ day6question3/question3.js
@@ -0,0 +1,33 @@
+const text = document.getElementById('text')
+
+const attr = document.getElementById('attributes');
+
+let name = prompt("enter the name");
+
+text.innerHTML= `Welcome ${name}`
+//text.style.color='red';
+text.style.color='white';
+
+attr.classList.toggle('bgdarkmode')
+
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
